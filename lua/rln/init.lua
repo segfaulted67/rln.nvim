975af50733806a53a045e3b43c73c18d977a050a
larry <segfaulted67@gmail.com>
2025-11-20 12:48:51 +0530

    fix: Added print statement

--- lua/rln/init.lua
+++ lua/rln/init.lua
@@ -3,6 +3,11 @@ local M = {}
 function M.toggle()
   local is_rln = vim.opt.relativenumber:get()
   vim.opt.relativenumber = not is_rln
+  if is_rln then
+    print("Disabled Relative Number")
+  else
+    print("Enabled Relative Number")
+  end,
 end
 
 vim.keymap.set({ 'n', 'v' }, '<leader>rn', function()
